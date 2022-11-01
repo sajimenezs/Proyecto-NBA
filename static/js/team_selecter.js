e6d420538b66a71342a61ae3b713916e2b9811ab
Gabriel Estupiñan <gamaespa@gmail.com>
2022-10-31 22:15:59 -0500

    Se solucionaron problemas de compatibilidad causados por el merge.

--- static/js/team_selecter.js
+++ static/js/team_selecter.js
@@ -1,26 +1,21 @@
-
-$(document).ready(function() {
-    $(".buscador-equipos").select2();
-});       
-
-
-function change_image(){
+function change_img(){
     var cod = document.getElementById("equipoA").value;
     
-    var new_image = " /static/images/logos/imagen_equipo_" + cod + ".png"
+    var new_image = " /static/images/logos/imagen_equipo_" + cod + ".png";
     
     document.getElementById("imagen-equipo-A").src=new_image;
     
 }
 
-function change_imageB(){
+function change_imgB(){
     var codB = document.getElementById("equipoB").value;
 
-    var new_imageB = " /static/images/logos/imagen_equipo_" + codB + ".png"
+    var new_imageB = " /static/images/logos/imagen_equipo_" + codB + ".png";
 
     document.getElementById("imagen-equipo-B").src=new_imageB;
 
 }
-  
 
-
+$(document).ready(function() {
+    $(".buscador-equipos").select2();
+}); 
